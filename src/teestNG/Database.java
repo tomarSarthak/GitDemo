1eb31cd0029fd87323db317688be5dd9a12bb590
tomarSarthak <51526390+tomarSarthak@users.noreply.github.com>
2021-05-30 00:40:36 +0530

    Update Database.java

--- src/teestNG/Database.java
+++ src/teestNG/Database.java
@@ -27,4 +27,18 @@ public class Database {
 		
 	}
 
+	 
+	@Test
+	public void jdbcconnection() throws SQLException
+	{
+		 Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/qadb", "root", "Root");
+		 Statement st = con.createStatement();
+		 ResultSet rs = st.executeQuery("Select * from employeinf where location= 'gwalior'");
+		 
+		 rs.next();	 
+		  String name = rs.getString("name");
+            System.out.println(name);		 
+
+		
+	}
 }
